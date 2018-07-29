63932cb2e98baff012166b3ef905bf0cb32f0dfb
eddyerburgh <edward.yerburgh@gmail.com>
2018-07-29 21:37:30 +0100

    init

---
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils';
+import HelloWorld from '../HelloWorld.vue';
+
+describe('HelloWorld.vue', () => {
+  test('renders props.msg when passed', () => {
+    const msg = 'new message';
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
